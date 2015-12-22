2fef18974bbae5ed338e262e259f7ea4da82dcb0
Santiago Baus <sbaus87@gmail.com>
2015-12-22 01:13:26 -0700

    Sockets work, problems were had due to port used

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(8080, function() {
+server.listen(3000, function () {
   console.log('listening');
 });
 
@@ -12,9 +12,9 @@ app.get('/', function(request, response){
   response.render(__dirname + '/public/index.ejs');
 });
 
-io.on('connection', function(socket){
-  socket.emit('news', {hello: "world"});
-  socket.on('key_move',function(data){
-    console.log("key pressed was " + data);
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('other_event', function (data) {
+    console.log(data);
   });
 });
