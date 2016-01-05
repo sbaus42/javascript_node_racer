3a9223d5cc49f8e48b44d3240efe6f439b470db2
Santiago Baus <sbaus87@gmail.com>
2016-01-05 15:30:48 -0700

    MVP-ish

--- public/lib/racer_logic.js
+++ public/lib/racer_logic.js
@@ -1,4 +1,19 @@
 var socket = io.connect('http://localhost:3000');
+socket.on('go_ahead', function (data){
+    if (data["command"] === 'move_right') {
+      console.log(data);
+      var player_1_active_index = $('#player-1 .active').index() + 1;
+      $('#player-1 td:nth-child('+ player_1_active_index +')').removeClass('active');
+      $('#player-1 td:nth-child('+(player_1_active_index + 1)+')').addClass('active');
+    }
+
+    if (data["command"] === 'move_left') {
+      console.log(data);
+      var player_2_active_index = $('#player-2 .active').index() + 1;
+      $('#player-2 td:nth-child('+ player_2_active_index +')').removeClass('active');
+      $('#player-2 td:nth-child('+(player_2_active_index + 1)+')').addClass('active');
+    }
+  });
 
 $(function(){
   // On keypress, emit message to server
@@ -11,24 +26,4 @@ $(function(){
       socket.emit('key_move', {command: "move_left"});
     }
   });
-
-  socket.on('go_ahead', function (data){
-    if (data["command"] === 'move_right') {
-      console.log(data);
-      var active_index = $('#player-1 .active').index() + 1;
-      $('#player-1 td:nth-child('+active_index+')').removeClass('active');
-      $('#player-1 td:nth-child('+(active_index + 1)+')').addClass('active');
-    }
-
-    if (data["command"] === 'move_left') {
-      console.log(data);
-      var active_index = $('#player-2 .active').index() + 1;
-      $('#player-2 td:nth-child('+active_index+')').removeClass('active');
-      $('#player-2 td:nth-child('+(active_index + 1)+')').addClass('active');
-    }
-
-      // $('#player-1 td:nth-child')
-  });
-
-  // On message from server, do something
 });
